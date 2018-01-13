19f1e630722b683b531fbe478a54ba5cfc7eb447
kurniadi <kurniadiahmadwijaya@gmail.com>
2018-01-13 13:04:16 +0700

    Calc Layout With Anko

--- AnkoKotlin/app/src/main/java/id/sch/mlg/smktelkom/learn/ankokotlinandroid/LayoutActivity.kt
+++ AnkoKotlin/app/src/main/java/id/sch/mlg/smktelkom/learn/ankokotlinandroid/LayoutActivity.kt
@@ -1,12 +1,58 @@
 package id.sch.mlg.smktelkom.learn.ankokotlinandroid
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_layout.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.sdk25.coroutines.onLongClick
 
 class LayoutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout)
+
+        btn_alert.onClick {
+
+            alert {
+             title = "Hallo Ini Tittle"
+
+                yesButton {
+                    toast("Hallo")
+                }
+                noButton {
+                    longToast("")
+                }
+
+            }.show()
+
+            // Alert Version 2
+
+            alert{
+                positiveButton("INI BUTTON POSITIF !!!!1!11!11", onClicked =
+                {
+
+                }
+                    )
+
+                negativeButton("INI BUTTON NEGATIF !!!!1!11!11", onClicked =
+                {
+
+                }
+                )
+
+
+
+            }
+        }
+
+        btn_load.onLongClick {
+            //Without Anko
+            var intent = Intent(Intent.ACTION_VIEW)
+            browse("http://github.com")
+        }
+
     }
 }
--- AnkoKotlin/app/src/main/java/id/sch/mlg/smktelkom/learn/ankokotlinandroid/MainActivity.kt
+++ AnkoKotlin/app/src/main/java/id/sch/mlg/smktelkom/learn/ankokotlinandroid/MainActivity.kt
@@ -3,22 +3,43 @@ package id.sch.mlg.smktelkom.learn.ankokotlinandroid
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+/*
+        // TODO 1 : Membuat View Group ( Jenis Layout )
+        verticalLayout{
+            // TODO 2 : Menambahkan View Edit Text
+            var input1 = editText {
+                hint = "Input Name"
+            }
+
+            var input2 = editText {
+                hint = "Input Alamat"
+            }
+
+            button{
+                text = "Submit"
+                onClick {
+                    toast(input1.text.toString() + input2.text.toString())
+                }
+            }
+        }
 
         // Button Listener
         btn_commons.onClick {
 
             /*
 
-             
+
             // Without Anko
             var intent = Intent(this@MainActivity , LayoutActivity::class.java)
             intent.putExtra("nilai", 12)
@@ -33,6 +54,55 @@ class MainActivity : AppCompatActivity() {
         btn_layout.onClick {
 
         }
+*/
+        verticalLayout{
+            var input1 = editText {
+                hint = "Masukkan Input 1"
+            }
+
+            var input2 = editText {
+                hint = "Masukkan Input 2"
+            }
+
+            button {
+                text = "+"
+                onClick {
+                    var hasil = input1.text.toString().toInt() + input2.text.toString().toInt()
+                    toast(hasil.toString())
+                }
+            }
+           button{
+               text = "-"
+               onClick {
+                   var hasil = input1.text.toString().toInt() - input2.text.toString().toInt()
+                   toast(hasil.toString())
+               }
+           }
+
+            button{
+                text = "x"
+                onClick {
+                    var hasil = input1.text.toString().toInt() * input2.text.toString().toInt()
+                    toast(hasil.toString())
+                }
+
+            }
+
+            button{
+                text = "/"
+                onClick {
+                   var hasil = input1.text.toString().toInt() / input2.text.toString().toInt()
+                    toast(hasil.toString())
+                }
+            }
+
+
+        }
+
+        linearLayout{
+            orientation = LinearLayout.HORIZONTAL
+        }
+
 
     }
 }
